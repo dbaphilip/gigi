75fc6fd40eb00b3205c4ca1b4a820b94a8172684
Mayowa <20-52ha047@students.unilorin.edu.ng>
2025-03-19 14:41:56 +0100

    Image Upload (1)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
-  before_action :require_sign_in, only: [:new]
-  before_action :set_product, only: [:show]
+  before_action :require_sign_in, only: [:new, :create, :edit, :update]
+  before_action :set_product, only: [:show, :edit, :update]
 
   def show
   end
@@ -19,10 +19,21 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
     def product_params
-      params.require(:product).permit(:title, :price, :description)
+      params.require(:product).permit(:title, :price, :image, :description)
     end
 
     def set_product
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ApplicationRecord
 
   belongs_to :user
 
+  has_one_attached :image
+
   validates :title, length: { minimum: 5 }
   validates :price, numericality: { greater_than: 100 }
   validates :description, length: { minimum: 30 }
